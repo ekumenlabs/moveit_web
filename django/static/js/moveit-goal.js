e5c6ad2e6870604bb45b0989e40f9a5939ace0da
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-08-27 14:43:07 -0300

    add direction to the goal

--- django/static/js/moveit-goal.js
+++ django/static/js/moveit-goal.js
@@ -1,3 +1,73 @@
+/**
+ * An Axes object can be used to display the axis of a particular coordinate frame.
+ *
+ * @constructor
+ * @param options - object with following keys:
+ *   * shaftRadius (optional) - the radius of the shaft to render
+ *   * headRadius (optional) - the radius of the head to render
+ *   * headLength (optional) - the length of the head to render
+ */
+Axes = function(options) {
+  var that = this;
+  options = options || {};
+  var shaftRadius = options.shaftRadius || 0.008;
+  var headRadius = options.headRadius || 0.023;
+  var headLength = options.headLength || 0.1;
+  var longLine = 1;
+  var shortLine = longLine * .5;
+
+  THREE.Object3D.call(this);
+
+  // create the cylinders for the objects
+  this.longLineGeom = new THREE.CylinderGeometry(shaftRadius, shaftRadius, longLine - headLength);
+  this.shortLineGeom = new THREE.CylinderGeometry(shaftRadius, shaftRadius, shortLine - headLength);
+  this.headGeom = new THREE.CylinderGeometry(0, headRadius, headLength);
+
+  function addAxis(axis, isLong) {
+    isLong = isLong || false;
+    var scalarFactor = isLong ? 0.95 : 0.48;
+
+    // set the color of the axis
+    var color = new THREE.Color();
+    color.setRGB(axis.x, axis.y, axis.z);
+    var material = new THREE.MeshBasicMaterial({
+      color : color.getHex()
+    });
+
+    // setup the rotation information
+    var rotAxis = new THREE.Vector3();
+    rotAxis.crossVectors(axis, new THREE.Vector3(0, -1, 0));
+    var rot = new THREE.Quaternion();
+    rot.setFromAxisAngle(rotAxis, 0.5 * Math.PI);
+
+    // create the arrow
+    var arrow = new THREE.Mesh(that.headGeom, material);
+    arrow.position = axis.clone();
+    arrow.position.multiplyScalar(scalarFactor);
+    arrow.useQuaternion = true;
+    arrow.quaternion = rot;
+    arrow.updateMatrix();
+    that.add(arrow);
+
+    // create the line
+    var line = new THREE.Mesh(isLong ? that.longLineGeom : that.shortLineGeom, material);
+    line.position = axis.clone();
+    line.position.multiplyScalar(scalarFactor / 2);
+    line.useQuaternion = true;
+    line.quaternion = rot;
+    line.updateMatrix();
+    that.add(line);
+  }
+
+  // add the three markers to the axes
+  addAxis(new THREE.Vector3(1, 0, 0), true);
+  addAxis(new THREE.Vector3(0, 1, 0));
+  addAxis(new THREE.Vector3(0, 0, 1));
+};
+Axes.prototype.__proto__ = THREE.Object3D.prototype;
+
+
+///////////////////////////////////////////////////////////////////////////////
 
 // @constructor
 //
@@ -15,10 +85,8 @@ MoveItGoal = function (pose, scene) {
   this.add(dot_mesh);
 
   // construct the axes, and keep a reference to show rotations
-  var axes = new ROS3D.Axes();
+  var axes = new Axes();
   this.add(axes);
-
-  // TODO: construct the direction arrow
 
   // add this goal to the general repository
   MoveItGoal.all.push(this);
