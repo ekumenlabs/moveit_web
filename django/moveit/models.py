02259499253fdc7cc31eab0573b3b3d75f9e51e3
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-08-28 13:07:29 -0300

    start working on db support for scenes

--- django/moveit/models.py
+++ django/moveit/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Pose (models.Model):
+    # To be used by Scene and GoalSet
+
+    # Denormalize the postion and orientation for simplicity
+
+    position_x = models.FloatField(default=0)
+    position_y = models.FloatField(default=0)
+    position_z = models.FloatField(default=0)
+
+    orientation_x = models.FloatField(default=0)
+    orientation_y = models.FloatField(default=0)
+    orientation_z = models.FloatField(default=0)
+    orientation_w = models.FloatField(default=0)
+
+
+class Scene (models.Model):
+    # Name of the scene, which fully identifies the Scene instance
+    name = models.CharField(max_length=20, primary_key=True)
+
+    # Mesh repository URL, relative to STATIC_URL
+    mesh_url = models.URLField()
+
+    # If the pose is null, the scene is empty
+    pose = models.ForeignKey(Pose, null=True)
