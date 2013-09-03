d74df7bcacb969d8508b4f7a5035d13451ab7f52
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-09-03 17:47:11 -0300

    allow to upload new scene

--- django/run.sh
+++ django/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Ensure the directory for uploads exists.
+[ -d '/home/vagrant/moveit/src/moveit_web/django/media' ] || mkdir -p '/home/vagrant/moveit/src/moveit_web/django/media'
+
+# Source the ROS environment.
 . ../../../devel/setup.bash
+
+# Launch the webserver.
 ./runserver.py
